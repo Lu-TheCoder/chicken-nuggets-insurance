9a5589393717530195609c908b7c4659833af54a
Buti Donovan Mkhwanazi <mkwanazibuti082@gmail.com>
2025-07-29 12:39:24 +0200

    Set up the routes, dashboard UI and alert component

---
+++ frontend/src/components/alert/Alert.tsx
@@ -0,0 +1,11 @@
+function Alert () {
+
+    return (
+        <div className="alert">
+            <h2>Alert</h2>
+            <p>I am Boitumelo</p>
+        </div>
+    )
+}
+
+export default Alert
